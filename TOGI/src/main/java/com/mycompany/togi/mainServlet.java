1a057377459a480c513538d6e16c0a438b67598d
Sterling Devins <sterling.devins@go.tarleton.edu>
2018-11-28 18:01:54 -0600

    Comment Box

--- TOGI/src/main/java/com/mycompany/togi/mainServlet.java
+++ TOGI/src/main/java/com/mycompany/togi/mainServlet.java
@@ -7,6 +7,8 @@ package com.mycompany.togi;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,16 +22,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "mainServlet", urlPatterns = {"/mainServlet"})
 public class mainServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    }
-
-
+    private final Map<String, Integer> menuItems = new LinkedHashMap<>();
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            menuItems.put("Hey", 1);
+            menuItems.put("Hi", 2);
+
+            for (Map.Entry<String, Integer> item : menuItems.entrySet()) {
+                out.printf("%s-%.2f%n", item.getKey(), item.getValue());
+            }
+        }
     }
-
-
 }
