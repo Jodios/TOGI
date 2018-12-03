839b669b82c16ea7fd5833ac0bcd25db9f07e416
Sterling Devins <sterling.devins@go.tarleton.edu>
2018-12-02 19:50:03 -0600

    Working on Thread AJAX

--- TOGI/src/main/java/com/mycompany/togi/mainServlet.java
+++ TOGI/src/main/java/com/mycompany/togi/mainServlet.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.togi;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import javax.servlet.ServletException;
