138d05a40c1f45e479520f6b988bee34017c78c1
SirSamhain <joelandapophis@gmail.com>
2018-12-04 21:55:53 -0600

    fixed board because it was bugging me :p

--- TOGI/src/main/java/com/mycompany/togi/Board.java
+++ TOGI/src/main/java/com/mycompany/togi/Board.java
@@ -12,14 +12,5 @@ public class Board {
 
     public void setCommentnum(Integer commentnum) {
         this.commentnum = commentnum;
-<<<<<<< HEAD
-   
     }
-
-    
-=======
-
-    }
-
->>>>>>> e8622e081b2edcc5f79af4de13141af3211cc0ab
 }
