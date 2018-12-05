d3ec40c9e240619f49ff6ae1fa309578c6025538
Sterling Devins <sterling.devins@go.tarleton.edu>
2018-12-04 21:07:23 -0600

    Fixed the board issue

--- TOGI/src/main/java/com/mycompany/togi/Board.java
+++ TOGI/src/main/java/com/mycompany/togi/Board.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.togi;
 
-/**
- *
- * @author scdev
- */
 public class Board {
-    
+
+    private Integer commentnum = 0;
+
+    public Integer getCommentnum() {
+        commentnum++;
+        return commentnum;
+
+    }
+
+    public void setCommentnum(Integer commentnum) {
+        this.commentnum = commentnum;
+
+    }
+
 }
